f5a363fb48ad29d32909ffcfe80da3623d01d023
AdriMendez <adri.noe.mendez@gmail.com>
2022-11-27 20:50:41 -0300

    m

---
+++ routes/products.js
@@ -0,0 +1,9 @@
+// crear las siguientes rutas:
+// 1 - /products (GET)  -  Listado de productos
+// 2 - /products/create (GET) - Formulario de creacion de productos
+// 3 - /products/:id (GET)  - Detalle de un producto en particular
+// 4 - /products (POST) - Accion de creación (a donde se envía el formulario)
+// 5 - /products/:id/edit (GET) - Formulario de Edición de un producto
+// 6 - /products/:id (PUT) - Acción de edición (a donde se envía el formulario)
+// 7 - /products/:id (DELETE) - Acción de borrado
+
