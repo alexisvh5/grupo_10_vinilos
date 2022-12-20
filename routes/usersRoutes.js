177f220fcc485d005e10e09a43f9b7e0df6decff
MarianoFunes <mgfg83@gmail.com>
2022-12-20 19:01:06 -0300

    validacion

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -14,7 +14,7 @@ const {body} = require ('express-validator');
 const validateCreateForm = [
     body ('NombreyApellido').notEmpty().withMessage('Debes completar el campo de Nombre y Apellido'),    
     body ('email').isEmail().withMessage('Agregar un email válido'),
-    body ('contrasena').notEmpty().withMessage('Debes completar el campo contraseña'),,
+    body ('contrasena').notEmpty().withMessage('Debes completar el campo contraseña'),
     body ('confirmacionContrasena').notEmpty().withMessage('Debes repetir la contraseña elegida'),
     body ('categoria').notEmpty().withMessage('Debes elegir una categoria'),
     body ('img').notEmpty().withMessage('Debes elegir tu foto de perfil'),
@@ -39,7 +39,7 @@ var upload = multer({storage: storage});
 
 //CREACION DE UN USUARIO - 
 router.get('/register', userController.register);
-router.post ('/register',upload.single('img'), userController.processRegister);
+router.post ('/register',upload.single('img'), validateCreateForm, userController.processRegister);
 
 
 // LOGEO DE UN USUARIO 
