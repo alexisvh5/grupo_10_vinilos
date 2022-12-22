1d77cedf2ffdebc572f95a727eed5ba0d3dd1dd5
alexis <alexis.herediav@hotmail.com>
2022-12-22 12:03:09 -0300

    combiancion en cambios

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
 let fs = require('fs');
 let path = require('path');
-//const bcrypt = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
 
 let pathUsersJson = path.join(__dirname, "../data/users.json");
 
 let users = JSON.parse(fs.readFileSync(pathUsersJson, 'UTF-8'));
+
 
 
 const { validationResult} = require ('express-validator');
@@ -18,17 +19,17 @@ const userController = {
     },
   
     processRegister: (req, res) => {
-      const resultValidation = validationResult(req);
+     /* const resultValidation = validationResult(req);
     
 
       if (resultValidation.errors.length > 0) {
         return res.render ('register'), {
         errors: resultValidation.mapped(),
       } 
-      }
-      },
-
-     /* let imagen;
+      }},*/
+      
+      
+      let imagen;
      // console.log(req.file)
     if(req.file != undefined) {
       imagen = req.file.filename;
@@ -39,12 +40,14 @@ const userController = {
     let newUser = {
       id: users[users.length - 1].id + 1,
       ...req.body,
+      contrasena:bcryptjs.hashSync(req.body.contrasena,10),
+      confirmacionContrasena:bcryptjs.hashSync(req.body.confirmacionContrasena,10),
       img:imagen, 
     };
     users.push(newUser)
     fs.writeFileSync(pathUsersJson, JSON.stringify(users, null, ' '));
-    res.redirect('/');  */
-  
+    res.redirect('/');  
+    },
 
   login: (req, res) => {
       return res.render ('login')
